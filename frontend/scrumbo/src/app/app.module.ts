8bd73992d9ebb3cf5d4ca4a68545d749adf741b2
Joel Balcaen <joel.balcaen@logmein.com>
2019-03-21 22:30:56 -0400

    fix import

--- frontend/scrumbo/src/app/app.module.ts
+++ frontend/scrumbo/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ShowDataComponent } from './show-data/show-data.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
