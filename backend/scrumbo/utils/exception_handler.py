476d5bce475b562c0513cab7f4de35d9e088769a
Joel Balcaen <joel.balcaen@logmein.com>
2019-04-29 21:11:35 -0400

    add migrations and utils folder, create custom exception handler and exception classes

---
+++ backend/scrumbo/utils/exception_handler.py
@@ -0,0 +1,20 @@
+from rest_framework.views import exception_handler
+from .exceptions import UniqueBoardException
+from rest_framework.response import Response
+
+
+def custom_exception_handler(exc, context):
+    # Call REST framework's default exception handler first,
+    # to get the standard error response.
+    response = exception_handler(exc, context)
+
+    # Now add the HTTP status code to the response.
+    if response is not None:
+        response.data['status_code'] = response.status_code
+
+    elif isinstance(exc, UniqueBoardException):
+        data = {'detail': exc.detail}
+        return Response(data, status=UniqueBoardException.status_code)
+
+
+    return response
---
+++ backend/scrumbo/utils/exceptions.py
@@ -0,0 +1,13 @@
+from rest_framework.exceptions import APIException
+from rest_framework import status
+
+
+class UniqueBoardException(APIException):
+    status_code = status.HTTP_409_CONFLICT
+    default_detail = 'This board name already exists. Please choose another one.'
+    default_code = 'existing_board_name'
+
+class InvalidBoardNameException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = 'Only alpha numberic characters and white spaces alowed in board name.'
+    default_code = 'invalid_character'
