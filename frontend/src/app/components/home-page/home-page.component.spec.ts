15e9f8a53a617a8eb0aa709abd3e63e67c418acf
Joel Balcaen <joel.balcaen@logmein.com>
2019-10-12 08:23:01 -0400

    home page tests

--- frontend/src/app/components/home-page/home-page.component.spec.ts
+++ frontend/src/app/components/home-page/home-page.component.spec.ts
@@ -1,14 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomePageComponent } from './home-page.component';
+import { BoardService } from 'src/app/services/board.service';
+import { Router } from '@angular/router';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { MatIconModule, MatError, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('HomePageComponent', () => {
+fdescribe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
+  let debugElement: DebugElement
+  let boardHttpServiceSpy: jasmine.SpyObj<BoardService>
+  let routerSpy: jasmine.SpyObj<Router>
 
   beforeEach(async(() => {
+    boardHttpServiceSpy = jasmine.createSpyObj('BoardService', ['getBoard']);
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
     TestBed.configureTestingModule({
-      declarations: [ HomePageComponent ]
+      declarations: [ HomePageComponent ],
+      providers: [
+        { provide: BoardService, useValue: boardHttpServiceSpy },
+        { provide: Router, useValue: routerSpy }
+      ],
+      imports: [
+        // BrowserAnimationsModule,
+        NoopAnimationsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule      ]
     })
     .compileComponents();
   }));
@@ -16,11 +39,18 @@ describe('HomePageComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HomePageComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement
     fixture.detectChanges();
   });
 
-  it('should contain head with the name of the application', () => {
+  it('should contain a couple of headers', () => {
     expect(component).toBeTruthy();
+
+    expect(debugElement.queryAll(By.css('h1')).length).toEqual(1)
+    expect(debugElement.queryAll(By.css('h2')).length).toEqual(1)
+    expect(debugElement.queryAll(By.css('form')).length).toEqual(1)
+    expect(debugElement.queryAll(By.css('button')).length).toEqual(1)
+
 
     
   });
--- frontend/src/app/components/home-page/home-page.component.ts
+++ frontend/src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 import { BoardService } from 'src/app/services/board.service';
 import { Router } from '@angular/router';
@@ -10,28 +10,30 @@ import { map, catchError } from 'rxjs/operators';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
-  
   boardForm = new FormGroup({
-  name: new FormControl('', [
+  name: new FormControl('',
+  [
     Validators.required,
-    Validators.maxLength(10),
-    Validators.pattern('[a-zA-Z0-9 ]+'),
-  ],[(value) => this.checkBoardNameTaken(value)])
-})
+    Validators.maxLength(30),
+    Validators.pattern('[a-zA-Z0-9 ]+')
+  ],
+    [(value) => this.checkBoardNameTaken(value)])
+  })
 
-constructor( private bs: BoardService, private router: Router) { 
-}
+  constructor( private bs: BoardService, private router: Router) {}
 
-onSubmit() {
-  this.bs.addBoard(this.boardForm.value.name).subscribe(
-    board => {
-      this.router.navigate([board.url_friendly_name])
-    })
-}
+  onSubmit() {
+    this.bs.addBoard(this.boardForm.value.name).subscribe(
+      board => {
+        this.router.navigate([board.url_friendly_name])
+      }
+    )
+  }
 
-checkBoardNameTaken(control: AbstractControl) {
-  return this.bs.checkNameNotTaken(control.value).pipe(
-    map(boardTaken => boardTaken ? { boardNameTaken: true } : null),
-    catchError(() => null)
-  )}
+  checkBoardNameTaken(control: AbstractControl) {
+    return this.bs.checkNameNotTaken(control.value).pipe(
+      map(boardTaken => boardTaken ? { boardNameTaken: true } : null),
+      catchError(() => null)
+    )
+  }
 }
