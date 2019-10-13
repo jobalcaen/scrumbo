8ec18dcc1c11699959fe456e6de3655b7b7504e9
Joel Balcaen <joel.balcaen@logmein.com>
2019-10-13 15:39:04 -0400

    home page tests

--- frontend/src/app/components/home-page/home-page.component.spec.ts
+++ frontend/src/app/components/home-page/home-page.component.spec.ts
@@ -5,12 +5,13 @@ import { BoardService } from 'src/app/services/board.service';
 import { Router } from '@angular/router';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
-import { MatIconModule, MatError, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { of } from 'rxjs';
+import { Board } from 'src/app/models/models';
 
-fdescribe('HomePageComponent', () => {
+describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
   let debugElement: DebugElement
@@ -18,7 +19,7 @@ fdescribe('HomePageComponent', () => {
   let routerSpy: jasmine.SpyObj<Router>
 
   beforeEach(async(() => {
-    boardHttpServiceSpy = jasmine.createSpyObj('BoardService', ['checkNameNotTaken']);
+    boardHttpServiceSpy = jasmine.createSpyObj('BoardService', ['checkNameNotTaken','addBoard']);
     routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
     TestBed.configureTestingModule({
@@ -29,7 +30,6 @@ fdescribe('HomePageComponent', () => {
       ],
       imports: [
         BrowserAnimationsModule,
-        // NoopAnimationsModule,
         ReactiveFormsModule,
         MatFormFieldModule,
         MatInputModule      ]
@@ -78,7 +78,7 @@ fdescribe('HomePageComponent', () => {
     expect(boardHttpServiceSpy.checkNameNotTaken).toHaveBeenCalledTimes(2)
   })
 
-  fit('displays an error if a board name is taken',() => {
+  it('displays an error if a board name is taken',() => {
     const nameInput = component.boardForm.controls['name']
 
     boardHttpServiceSpy.checkNameNotTaken.and.returnValue(of(true))
@@ -90,14 +90,64 @@ fdescribe('HomePageComponent', () => {
 
     expect(nameInput.errors['boardNameTaken']).toBeTruthy()
 
-    fixture.detectChanges()
-    expect(debugElement.query(By.css('button')).nativeElement.disabled).toBeTruthy()
-    expect(debugElement.query(By.css('mat-error')).nativeElement.text).toEqual('This board name has already been taken')
-
-    // // alpha numeric characters only
-    // component.boardForm.controls['name'].setValue('%')
-    // expect(component.boardForm.valid).toBeFalsy()
-
+    
+    // TODO: assert the error messages are in the DOM
+    // fixture.detectChanges()
+    // expect(debugElement.query(By.css('button')).nativeElement.disabled).toBeTruthy()
+    // expect(debugElement.query(By.css('mat-error')).nativeElement.text).toEqual('This board name has already been taken')
 
   })
-});
+
+  it('displays an error if a non alpha-numeric character is entered',() => {
+    const nameInput = component.boardForm.controls['name']
+
+    boardHttpServiceSpy.checkNameNotTaken.and.returnValue(of(false))
+
+    expect(nameInput.errors['boardNameTaken']).toBeFalsy()
+    expect(nameInput.errors['pattern']).toBeFalsy()
+
+    nameInput.setValue('a')
+    expect(nameInput.valid).toBeTruthy()
+
+    nameInput.setValue('a%')
+    expect(nameInput.errors['boardNameTaken']).toBeFalsy()
+    expect(nameInput.errors['pattern']).toBeTruthy()
+
+
+    expect(nameInput.valid).toBeFalsy()
+
+    
+    // TODO: assert the error messages are in the DOM
+    // fixture.detectChanges()
+    // expect(debugElement.query(By.css('button')).nativeElement.disabled).toBeTruthy()
+    // expect(debugElement.query(By.css('mat-error')).nativeElement.text).toEqual('This board name has already been taken')
+
+  })
+
+  it('send the board name to the board service and navigate to the newly created board when the submit button is clicked',() => {
+    const sampleBoard: Board = {
+      id: 222,
+      name: 'the hello',
+      url_friendly_name: 'the-hello'
+
+    }
+    const nameInput = component.boardForm.controls['name']
+
+    boardHttpServiceSpy.checkNameNotTaken.and.returnValue(of(false))
+    boardHttpServiceSpy.addBoard.and.returnValue(of(sampleBoard))
+
+    nameInput.setValue(sampleBoard.name)
+    component.onSubmit()
+
+    expect(boardHttpServiceSpy.addBoard).toHaveBeenCalledTimes(1)
+    expect(boardHttpServiceSpy.addBoard).toHaveBeenCalledWith('hello')
+    expect(routerSpy.navigate).toHaveBeenCalledTimes(1)
+    expect(routerSpy.navigate).toHaveBeenCalledWith(sampleBoard.url_friendly_name)
+    
+    // TODO: convert this to an async test and use the button in the DOM
+
+  })
+
+
+  
+})
