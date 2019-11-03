b04a6571b51af3c7deb09cff776e058670546b21
Joel Balcaen <joel.balcaen@logmein.com>
2019-11-02 21:49:07 -0400

    update docker compose

--- backend/my_app/settings.py
+++ backend/my_app/settings.py
@@ -17,7 +17,7 @@ DEBUG = int(os.environ.get('DEBUG', default=0))
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders',
+    # 'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    # 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
