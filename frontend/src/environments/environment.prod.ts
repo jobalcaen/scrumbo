a040790497ae214c3614a4050ebb225675b30a87
Joel Balcaen <joel.balcaen@logmein.com>
2019-11-17 19:01:34 -0500

    update environment file to that is does not include a specific protocol build files

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  httpApiUrl: 'http://0.0.0.0:8000/api/boards',
-  wsUrl: 'ws://0.0.0.0:8000'
+  // httpApiUrl: 'http://0.0.0.0:8000/api/boards',
+  // wsUrl: 'ws://0.0.0.0:8000'
+  httpApiUrl: '//127.0.0.1:8000/api/boards',
+  wsUrl: '//127.0.0.1:8000'
 }
