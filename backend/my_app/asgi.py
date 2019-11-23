df5d4805900786e96414381a4f558886a463c0e8
Joel Balcaen <joel.balcaen@logmein.com>
2019-11-23 12:54:25 -0500

    modify frontend environments prod url

--- backend/my_app/asgi.py
+++ backend/my_app/asgi.py
@@ -1,12 +1,3 @@
-# import os
-# from channels.asgi import get_channel_layer
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_app.settings")
-
-# channel_layer = get_channel_layer()
-
-
-
 import os
 import django
 from channels.routing import get_default_application
