e5b4a61e38f3f06a895274196594549f1128f3da
Joel Balcaen <joel.balcaen@logmein.com>
2019-11-23 12:03:59 -0500

    daphne working locally@!@@@

--- backend/my_app/asgi.py
+++ backend/my_app/asgi.py
@@ -1,8 +1,16 @@
-import os
+# import os
+# from channels.asgi import get_channel_layer
 
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_app.settings")
+
+# channel_layer = get_channel_layer()
+
+
+
+import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_app.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_app.settings")
 django.setup()
 application = get_default_application()
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   httpApiUrl: '/api/boards',
-  wsUrl: 'wss://127.0.0.1:8888'
+  wsUrl: 'ws://127.0.0.1:8000'
 }
