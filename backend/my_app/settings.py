d6e04e6426b4ea7a2d57f2bdb1f262d8d205b1f9
Joel Balcaen <joel.balcaen@logmein.com>
2019-11-23 18:12:03 -0500

    remove conn max age from dj_database_url

--- backend/my_app/settings.py
+++ backend/my_app/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 print('DB URL: ', DATABASE_URL)
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config()
 
 AUTH_PASSWORD_VALIDATORS = [
     {
